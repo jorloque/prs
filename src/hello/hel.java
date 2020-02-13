c07982767785ce38d5bf43027f45e14eb815aab1
jorloque <jorloque@gmail.com>
2020-02-13 19:39:52 +0100

    mi primer commit

---
+++ src/hello/hel.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class hel {
+
+	public hel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("bvdskjv");
+
+	}
+
+}
